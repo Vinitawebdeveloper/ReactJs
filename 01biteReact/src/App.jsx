713c8bde62fbe8bb19dcfc9ded1c4178f5ffc8dc
Vinitawebdeveloper <96649988+Vinitawebdeveloper@users.noreply.github.com>
2023-10-23 12:37:48 +0530

    created React App

---
+++ 01biteReact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+  <h1>React With Vite | vinita</h1>
+  )
+}
+
+export default App
---
+++ basics/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>React Js | vinita</h1>
+  );
+}
+
+export default App;
