af6ef418c454453f6e59c5f7182c83621218a785
Vinitawebdeveloper <96649988+Vinitawebdeveloper@users.noreply.github.com>
2023-11-02 14:23:58 +0530

    thirdProject

---
+++ 01basiccvite/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+// what is reconcialation 
+// the algorithm react uses to diff one tree with another to determine whish parts to be changed 
